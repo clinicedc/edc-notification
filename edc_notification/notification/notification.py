42c3cce0b440a89c20f2db8228cc106dcec4d5fb
erikvw <ew2789@gmail.com>
2018-11-11 15:42:58 -0600

    remove mailing list funcs from main notification class, clean up template options and passing of options into send methods

--- edc_notification/notification/notification.py
+++ edc_notification/notification/notification.py
@@ -1,12 +1,11 @@
-from copy import copy
 from django.apps import apps as django_apps
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import EmailMessage
+from edc_base.utils import get_utcnow
 from twilio.base.exceptions import TwilioRestException, TwilioException
 from twilio.rest import Client
 
-from ..mailing_list_creator import MailingListCreator
 from ..site_notifications import site_notifications
 from ..sms_message import SmsMessage
 
@@ -18,13 +17,12 @@ class NotificationError(Exception):
 class Notification:
 
     app_name = None
-    name = 'undefined'
+    name = None
     display_name = None
 
     email_from = settings.EMAIL_CONTACTS.get('data_manager')
     email_to = None
     email_message_cls = EmailMessage
-    mailing_list_manager_cls = MailingListCreator
 
     email_body_template = (
         '\n\nDo not reply to this email\n\n'
@@ -42,6 +40,13 @@ class Notification:
         '{test_subject_line}{protocol_name}: '
         '{display_name} '
         'for {subject_identifier}')
+    email_footer_template = (
+        '\n\n-----------------\n'
+        'To unsubscribe remove "{display_name}" from your chosen '
+        'email notifications in your user profile.\n\n'
+        '{name}\n'
+        '{notification_reason}:{instance.pk}\n'
+        '{message_datetime} (UTC)')
     email_test_body_line = 'THIS IS A TEST MESSAGE. NO ACTION IS REQUIRED\n\n'
     email_test_subject_line = 'TEST/UAT -- '
 
@@ -65,10 +70,10 @@ class Notification:
         if not live_system:
             self.email_to = [f'test.{email}' for email in self.email_to]
             self.test_message = True
-        self.mailing_list_manager = self.mailing_list_manager_cls(
-            email_to=self.email_to,
-            display_name=self.display_name,
-            name=self.name)
+
+    def __repr__(self):
+        return (f'<{self.__class__.__name__}:name=\'{self.name}\', '
+                f'display_name=\'{self.display_name}\'>')
 
     def __str__(self):
         return f'{self.name}: {self.display_name}'
@@ -77,7 +82,8 @@ class Notification:
     def default_email_to(self):
         return [f'{self.name}.{settings.APP_NAME}@mg.clinicedc.org']
 
-    def notify(self, force_notify=None, use_email=None, use_sms=None, **kwargs):
+    def notify(self, force_notify=None, use_email=None, use_sms=None,
+               email_body_template=None, **kwargs):
         """Notify / send an email and/or SMS.
 
         Main entry point.
@@ -96,13 +102,17 @@ class Notification:
         use_sms = use_sms or getattr(settings, 'TWILIO_ENABLED', False)
         if force_notify or self._notify_on_condition(**kwargs):
             if use_email:
-                email_sent = self.send_email(**kwargs)
+                email_body_template = (
+                    email_body_template + self.email_footer_template)
+                email_sent = self.send_email(
+                    email_body_template=email_body_template, **kwargs)
             if use_sms:
                 sms_sent = self.send_sms(**kwargs)
             self.post_notification_actions(
                 email_sent=email_sent,
                 sms_sent=sms_sent,
                 **kwargs)
+        return True if email_sent or sms_sent else False
 
     def notify_on_condition(self, **kwargs):
         """Override to return True if the notification
@@ -119,7 +129,7 @@ class Notification:
             return test_message or self.notify_on_condition(**kwargs)
         return False
 
-    def post_notification_actions(self, email_sent=None, sms_sent=None):
+    def post_notification_actions(self, **kwargs):
         pass
 
     @property
@@ -148,41 +158,41 @@ class Notification:
             self._notification_enabled = obj.enabled
         return self._notification_enabled
 
-    @property
-    def template_options(self):
+    def get_template_options(self, instance=None, test_message=None, **kwargs):
         """Returns a dictionary of message template options.
 
         Extend using `extra_template_options`.
         """
-        protocol_name = django_apps.get_app_config(
-            'edc_protocol').protocol_name
+        protocol_name = django_apps.get_app_config('edc_protocol').protocol_name
+        test_message = test_message or self.test_message
         template_options = dict(
             name=self.name,
             protocol_name=protocol_name,
             display_name=self.display_name,
-            email_from=self.email_from)
-        template_options.update(**self.extra_template_options)
-        return template_options
-
-    @property
-    def extra_template_options(self):
-        """Override to return a dictionary of extra message
-        template options.
-
-        See also: `template_options`
-        """
-        return {}
-
-    def send_email(self, fail_silently=None, email_to=None, **kwargs):
-        test_message = self.test_message or kwargs.get('test_message')
-        opts = copy(self.template_options)
-        opts.update(
+            email_from=self.email_from,
             test_subject_line=(
                 self.email_test_subject_line if test_message else '').strip(),
-            test_body_line=self.email_test_body_line if test_message else '')
-        opts.update(**kwargs)
-        subject = self.email_subject_template.format(**opts)
-        body = self.email_body_template.format(**opts)
+            test_body_line=self.email_test_body_line if test_message else '',
+            test_line=self.sms_test_line if test_message else '',
+            message_datetime=get_utcnow())
+        if 'subject_identifier' not in template_options:
+            try:
+                template_options.update(
+                    subject_identifier=instance.subject_identifier)
+            except AttributeError:
+                pass
+        if 'site_name' not in template_options:
+            try:
+                template_options.update(site_name=instance.site.name.title())
+            except AttributeError:
+                pass
+        return template_options
+
+    def send_email(self, fail_silently=None, email_to=None,
+                   email_body_template=None, **kwargs):
+        kwargs.update(**self.get_template_options(**kwargs))
+        subject = self.email_subject_template.format(**kwargs)
+        body = (email_body_template or self.email_body_template).format(**kwargs)
         args = [
             subject, body,
             self.email_from,
@@ -192,13 +202,9 @@ class Notification:
 
     def send_sms(self, fail_silently=None, sms_recipient=None, **kwargs):
         status = {}
-        test_message = self.test_message or kwargs.get('test_message')
         if self.sms_sender:
-            opts = copy(self.template_options)
-            opts.update(
-                test_line=self.sms_test_line if test_message else '',
-                **kwargs)
-            body = self.sms_template.format(**opts)
+            kwargs.update(**self.get_template_options(**kwargs))
+            body = self.sms_template.format(**kwargs)
             try:
                 client = Client()
             except (TwilioRestException, TwilioException):
