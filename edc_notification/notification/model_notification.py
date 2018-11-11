2e8b32fe07dafd82c2a0e4fa6418c616b0f7a380
erikvw <ew2789@gmail.com>
2018-11-11 15:40:48 -0600

    notify if model matches notification.model

--- edc_notification/notification/model_notification.py
+++ edc_notification/notification/model_notification.py
@@ -29,17 +29,40 @@ class ModelNotification(Notification):
         'at site {instance.site.name} may require '
         'your attention. Login to review. (See your user profile to unsubscribe.)')
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self):
+        super().__init__()
         if not self.display_name:
             self.display_name = django_apps.get_model(
                 self.model)._meta.verbose_name.title()
 
-    def post_notification_actions(self, instance=None, **kwargs):
-        pass
+    def __repr__(self):
+        return (f'<{self.__class__.__name__}:name=\'{self.name}\', '
+                f'display_name=\'{self.display_name}\','
+                f'model=\'{self.model}\'>')
+
+    def __str__(self):
+        return f'{self.name}: {self.display_name} ({self.model})'
+
+    def notify(self, force_notify=None, use_email=None, use_sms=None, **kwargs):
+        """Overridden to only call `notify` if model matches.
+        """
+        notified = False
+        instance = kwargs.get('instance')
+        try:
+            model = instance._meta.label_lower
+        except AttributeError:
+            pass
+        else:
+            if model == self.model:
+                notified = super().notify(
+                    force_notify=force_notify,
+                    use_email=use_email,
+                    use_sms=use_sms, **kwargs)
+        return notified
 
     @property
     def test_template_options(self):
+
         class Site:
             domain = 'gaborone.example.com'
             name = 'gaborone'
