a9a6a4def2d705028bff92fe0b9bfd0ded3389b5
erikvw <ew2789@gmail.com>
2018-11-11 15:37:38 -0600

    refine notifiy condition

--- edc_notification/notification/graded_event_notification.py
+++ edc_notification/notification/graded_event_notification.py
@@ -7,13 +7,17 @@ class GradedEventNotification(ModelNotification):
     model = None
 
     def notify_on_condition(self, instance=None, **kwargs):
-        if instance._meta.label_lower == self.model:
-            grading_history = [
-                int(obj.ae_grade)
-                for obj in instance.history.all().order_by('history_date')]
-            grading_history.reverse()
-            if grading_history:
-                x = [int(x) for x in grading_history]
-                if sum(x) != self.grade * 2 and x[0] == self.grade:
-                    return True
-        return False
+        notify_on_condition = False
+        history = [
+            int(obj.ae_grade)
+            for obj in instance.history.all().order_by('history_date')]
+        history.reverse()
+        if history:
+            try:
+                last_grade = history[1]
+            except IndexError:
+                notify_on_condition = history[0] == self.grade
+            else:
+                notify_on_condition = (
+                    history[0] == self.grade and last_grade != history[0])
+        return notify_on_condition
