f80935b0fe3f5efb1c38d4fab6bfdc3cd6e25d6b
erikvw <ew2789@gmail.com>
2019-05-21 15:43:26 -0500

    silent operatinal error in system check

--- edc_notification/system_checks.py
+++ edc_notification/system_checks.py
@@ -1,9 +1,9 @@
 import sys
 
-from django.apps import apps as django_apps
+from django.contrib.auth import get_user_model
 from django.core.checks import Warning, register
 from django.db.models import Q
-from django.db.utils import ProgrammingError
+from django.db.utils import ProgrammingError, OperationalError
 
 
 @register()
@@ -11,9 +11,8 @@ def edc_notification_check(app_configs, **kwargs):
     errors = []
     try:
         if "migrate" not in sys.argv and "makemigrations" not in sys.argv:
-            User = django_apps.get_model("auth.user")
-
-            for user in User.objects.filter(
+            User = get_user_model()
+            users = User.objects.filter(
                 (
                     Q(first_name__isnull=True)
                     | Q(last_name__isnull=True)
@@ -21,18 +20,22 @@ def edc_notification_check(app_configs, **kwargs):
                 ),
                 is_active=True,
                 is_staff=True,
-            ):
-                errors.append(
-                    Warning(
-                        (
-                            f"User account is incomplete. Check that first name, "
-                            f"last name and email are complete. See {user}"
-                        ),
-                        hint="Complete the user's account details.",
-                        obj=User,
-                        id="edc_notification.W001",
+            )
+            try:
+                for user in users:
+                    errors.append(
+                        Warning(
+                            (
+                                f"User account is incomplete. Check that first name, "
+                                f"last name and email are complete. See {user}"
+                            ),
+                            hint="Complete the user's account details.",
+                            obj=User,
+                            id="edc_notification.W001",
+                        )
                     )
-                )
+            except OperationalError:
+                pass
     except ProgrammingError:
         pass
     return errors
