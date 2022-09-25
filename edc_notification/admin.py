29f1c2dadfa9070311a2b102c1435f998da64774
erikvw <ew2789@gmail.com>
2022-09-24 22:46:32 -0500

    add TemplatesModelAdminMixin

--- edc_notification/admin.py
+++ edc_notification/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import register
+from edc_model_admin.mixins import TemplatesModelAdminMixin
 
 from .admin_site import edc_notification_admin
 from .models import Notification
 
 
 @register(Notification, site=edc_notification_admin)
-class NotificationAdmin(admin.ModelAdmin):
+class NotificationAdmin(TemplatesModelAdminMixin, admin.ModelAdmin):
 
     model = Notification
 
